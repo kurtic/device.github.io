caddbbaeb7a13a36f892c00997f92250f15e248a
kurtic <54854704+kurtic@users.noreply.github.com>
2019-10-17 14:38:21 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,49 @@
+	$(document).ready(function(){
+			$('.menu-toggle').click(function(){
+				$('nav').toggleClass('active')
+				
+			})
+	
+			$('#list').click(function(){
+				$('form').toggleClass('active1')
+	
+			})
+
+		
+			$('#cancel').click(function(){
+				$('form').toggleClass('active1')
+			})
+
+			$('#light1').click(function(){
+				
+				$('#styleslight').attr('href','styleslight.css')
+				
+			})
+
+			$('#dark').click(function(){
+				
+				$('#styleslight').attr('href','styles.css')
+				
+			})
+
+			$('#E-mail').blur(function()          //whenever you click off an input element
+				{                   
+   				 if( !$(this).val() ) {                      //if it is blank. 
+         				alert('Введіть E-mail!');    
+   				}
+				});
+			$('#password').blur(function()          //whenever you click off an input element
+				{                   
+   				 if( !$(this).val() ) {                      //if it is blank. 
+         				alert('Введіть пароль!');    
+   				}
+				});
+			 // enter.addEventListener("contextmenu" , () =>alert('Контекстне меню'));
+			 // enter.addEventListener("click" , () =>alert('Ви ввійшли в свій кабінет'));
+			 // serv.addEventListener("mousemove" , () =>alert('Ви провели мишкою через "Сервіси"'));
+			 // trans.addEventListener("transitionend" , () =>alert('Анімація завершена'));
+		})
+
+
+		
+		
